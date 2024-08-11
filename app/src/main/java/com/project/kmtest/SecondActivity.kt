5e156cabd7ad684f345c6d7614fc093eb273dcae
farhanfath <farhanfathur73@gmail.com>
2024-08-12 03:58:41 +0700

    style: adding first template UI

--- app/src/main/java/com/project/kmtest/SecondActivity.kt
+++ app/src/main/java/com/project/kmtest/SecondActivity.kt
@@ -1,21 +1,20 @@
 package com.project.kmtest
 
-import android.app.AlertDialog
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.project.kmtest.databinding.ActivityMainBinding
+import com.project.kmtest.databinding.ActivitySecondBinding
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivitySecondBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
--- app/src/main/java/com/project/kmtest/ThirdActivity.kt
+++ app/src/main/java/com/project/kmtest/ThirdActivity.kt
@@ -1,21 +1,19 @@
 package com.project.kmtest
 
-import android.app.AlertDialog
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.project.kmtest.databinding.ActivityMainBinding
+import com.project.kmtest.databinding.ActivityThirdBinding
 
-class MainActivity : AppCompatActivity() {
+class ThirdActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityThirdBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityThirdBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
